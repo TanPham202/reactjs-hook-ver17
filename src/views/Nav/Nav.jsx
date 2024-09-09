faa4d09bb4b9b6cab95318b34677e97e68e518b4
Tan Pham <tann4878@gmail.com>
2024-09-09 10:24:28 +0700

    #14: React Router - Link & Navlink

--- src/views/Nav/Nav.jsx
+++ src/views/Nav/Nav.jsx
@@ -1,12 +1,15 @@
+// Phiên bản React 18 sẽ bị lỗi, cần reload lại trang
 import './Nav.scss';
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
     return (
         <div className="topnav">
-            {/* Các href của thẻ a phải giống path bên App.js */}
-            <a className="active" href="/"> Home </a>
-            <a href="/page-1"> Page 1 </a>
-            <a href="/page-2"> Page 2 </a>
+            {/* Các to của thẻ NavLink phải giống path bên App.js */}
+            {/* exact=true là để xác định lúc đầu chọn tab nào */}
+            <NavLink to="/" activeClassName="active" exact={true}> Home </NavLink>
+            <NavLink to="/page-1" activeClassName="active"> Page 1 </NavLink>
+            <NavLink to="/page-2" activeClassName="active"> Page 2 </NavLink>
         </div>
     );
 }
