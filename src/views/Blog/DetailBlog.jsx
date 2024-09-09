ec5ab7a03bbc8f7a5d48f6e6274537411d9a71c8
Tan Pham <tann4878@gmail.com>
2024-09-09 10:44:04 +0700

    #17: React Router - Route Parameters - useParams 2

--- src/views/Blog/DetailBlog.jsx
+++ src/views/Blog/DetailBlog.jsx
@@ -1,11 +1,29 @@
 import { useParams } from 'react-router-dom';
+import './Blog.scss';
+import useFetch from "./useFetch";
 
 const DetailBlog = () => {
     // Lấy tham số trên URL gán vào biến "id"
     // id phải giống với route bên App
     let {id} = useParams();
+
+    // Custom Hook
+    const { dataLocal: dataDetailBlog, loading } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+
     return (
-        <h1> Hello detail blogs with id = {id} </h1>
+        <>
+            <h1> Hello detail blogs with id = {dataDetailBlog.id} </h1>
+            <div className="blogs-container">
+                { loading === false && dataDetailBlog ?
+                    <div className="single-blog">
+                        <div className="title"> {dataDetailBlog.title} </div>
+                        <div className="content"> {dataDetailBlog.body} </div>
+                    </div>
+                    :
+                    <div style={{textAlign: 'center !important'}}> Loading data... </div>
+                }
+            </div>
+        </>
     )
 }
 
