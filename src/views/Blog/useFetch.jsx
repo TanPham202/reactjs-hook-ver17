9ac761f18b3f363385b09709d63094c1366073d9
Tan Pham <tann4878@gmail.com>
2024-09-09 10:52:24 +0700

    #19: useEffect Cleanup

--- src/views/Blog/useFetch.jsx
+++ src/views/Blog/useFetch.jsx
@@ -7,13 +7,38 @@ const useFetch = (url) => {
     const [dataLocal, setDataLocal] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    const getDataAPI = async() => {
-        setTimeout(async () => {
-            let res = await axios.get(url)
-            let data = res && res.data ? res.data : [];
-            setDataLocal(data)
-            setLoading(false);
+    const getDataAPI = () => {
+        const ourRequest = axios.CancelToken.source() // Tạo cancel token <-- 1st step
+
+        const fetchData = async() => {
+            try{
+                // API lấy được gán vào biến res + Gán cancel token vào request muốn cancel
+                let res = await axios.get(url, { 
+                    cancelToken: ourRequest.token // <-- 2nd step
+                })
+                let data = res && res.data ? res.data : []; 
+                setDataLocal(data)
+                setLoading(false);
+            }
+            catch (err){
+                // Nếu cancel thành công thì in ra "Request canceled" và ngưng gửi request
+                // Nếu cancel thất bại thì gửi request thành công , thay đổi giá trị State Loading
+                if (axios.isCancel(err)) {
+                    console.log('Request canceled', err.message);
+                } else {
+                    setLoading(false);
+                }
+            }
+        }
+
+        setTimeout(() => {
+            fetchData();
         },3000)
+
+         // Trả về thông báo cancel <-- 3rd step
+        return () => {
+            ourRequest.cancel('Operation canceled by the user.')
+        }
     }
 
     useEffect(() =>{
