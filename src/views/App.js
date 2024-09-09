d42e4f73fd310129bdd75d8fa311e1290e30fd01
Tan Pham <tann4878@gmail.com>
2024-09-09 10:49:08 +0700

    #18: 404 Not Found

--- src/views/App.js
+++ src/views/App.js
@@ -4,6 +4,7 @@ import Nav from './Nav/Nav'
 import Home from './Home';
 import Blog from './Blog/Blog';
 import DetailBlog from './Blog/DetailBlog';
+import NotFound from './NotFound';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom"; // Thêm từ thư viện React Router
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
                 <Route path= "/" exact> <Home /> </Route>
                 <Route path = "/blog" exact> <Blog /> </Route>
                 <Route path = "/blog/:id"> <DetailBlog /> </Route> {/* :id đặt là gì cũng được */}
+                <Route path = "*" exact> <NotFound /> </Route>
             </Switch>
         </header>
       </div>
---
+++ src/views/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <>
+            <h5> 404 NOT FOUND </h5>
+            <div> Không thể tìm thấy trang này. Vui lòng thử lại sau </div>
+        </>
+    );
+}
+
+export default NotFound;
