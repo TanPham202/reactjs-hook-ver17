4162938c0c260222dcf974c633f3ae36c1494626
Tan Pham <tann4878@gmail.com>
2024-09-09 09:32:56 +0700

    #9: [Library] Axios with ReactJS

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
-import DemoUseEffect from './DemoUseEffect';
+import DemoAxios from './DemoAxios';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <DemoUseEffect />
+        <DemoAxios />
       </header>
     </div>
   );
