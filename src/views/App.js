fb78fcfc160077d8ea27cfa17e63eafd8b131056
Tan Pham <tann4878@gmail.com>
2024-09-09 09:06:11 +0700

    #6: Outputting List

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
-import DemoProps from './DemoProps';
+import DemoOutputtingLists from './DemoOutputtingLists';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <DemoProps />
+        <DemoOutputtingLists />
       </header>
     </div>
   );
